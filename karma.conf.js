7002a8882564a1094a89705a3603a18aa8798ff0
RUD SK8 <RUD SK8>
2017-01-23 22:46:12 -0200

    Estudo sobre testes em Karma

---
+++ karma.conf.js
@@ -0,0 +1,19 @@
+module.exports = function (config) {
+	config.set({
+		basePath: '',
+		frameworks: ['jasmine'],
+		files: [
+			'angular.min.js',
+			'angular-mocks.js',
+			'controller.js',
+			'simpleSpec.js',
+			'controllerSpec.js'
+		],
+		exclude: [],
+		port: 8080,
+		logLevel: config.LOG_INFO,
+		autoWatch: true,
+		browsers: ['Chrome'],
+		singleRun: false
+	});
+};
